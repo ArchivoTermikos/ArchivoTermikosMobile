1f77e654da9acb8a6e2328cbffba687d53f65eb7
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-02-26 10:38:26 +0100

    Inicial

---
+++ app/src/main/java/com/termikos/archivotermikosmobile/AulasActivity.java
@@ -0,0 +1,45 @@
+package com.termikos.archivotermikosmobile;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.termikos.archivotermikosmobile.adapters.AulasAdapter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AulasActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_aulas);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        List<String> aulas = Arrays.asList(new String[]{"Aula 1", "Aula 2", "Aula 3"});
+        recyclerView.setAdapter(new AulasAdapter(aulas));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
